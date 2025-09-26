80e1322e07517e4c3aae8537bb1870b20d3d5e16
khoureich <118670434+khoureich@users.noreply.github.com>
2025-09-26 18:26:20 +0000

    Update run_CIBE2.py

--- run_CIBE2.py
+++ run_CIBE2.py
@@ -4,6 +4,8 @@
 | Date:         09/2025
 |
 '''
+import random
+import string
 
 from charm.toolbox.pairinggroup import PairingGroup, ZR
 from charm.toolbox.pairinggroup import G1
@@ -13,10 +15,19 @@ def main():
     pairing_group = PairingGroup('MNT224')
     
     # the cluster size or the functionality parameter of the IPFE 
-    l = 21
-    
+    l = 22
+
+    # the number of IDs we want to manage
+    nID = 10000
+
+    # We determined the number of clusters experimentally
+    # When we set nID=10000 and nClusters=1250, we get that the number
+    # of vectors in a cluster varies between 1.0 and 19.0 (< l-1 = 21)
+    # with a mean of 8.00 and a standard deviation of 2.83.
+    nClusters = 1250
+
     # create an instance
-    cibe2 = CIBE2(pairing_group, l)
+    cibe2 = CIBE2(pairing_group, l, nID, nClusters)
     
     # setup
     (MSK, params) = cibe2.setup()
